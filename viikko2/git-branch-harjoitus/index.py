e66f3091ff7989e5ad7fe6acf3a9a460b186b2c0
Kooville <ville.kuhlberg@helsinki.fi>
2025-11-10 23:41:41 +0200

    muutos mainiin

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -1,10 +1,11 @@
 # tehdään alussa importit
 
+
 from logger import logger
 from summa import summa
 from erotus import erotus
 
-logger("aloitetaan")
+logger("aloitetaan ohjelma") # muutos mainissa
 
 x = int(input("luku 1: "))
 y = int(input("luku 2: "))
