8a05fb3efe09a066d144fa58ccd524c7a78886e2
Kooville <ville.kuhlberg@helsinki.fi>
2025-11-10 21:34:35 +0200

    Tehtävä 5

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,15 +1,25 @@
 from PlayerReader import PlayerReader
 from PlayerStats import PlayerStats
+from printing import Printer
+from rich  import print
+from rich.console import Console
+
+console = Console()
 
 def main():
-    url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
-    reader = PlayerReader(url)
-    stats = PlayerStats(reader)
-    players = stats.top_scorers_by_nationality("FIN")
+    while True:
+        season = console.input("Enter season [cyan](2018-19/2019-20/2020-21/2021-22/2022-23/2023-24/2024-25/2025-26)[/cyan] [magenta]blank to stop:[/magenta]")
+        if season == "":
+            break
+        url = f"https://studies.cs.helsinki.fi/nhlstats/{season}/players"
+        reader = PlayerReader(url)
+        stats = PlayerStats(reader)
+        nationality = console.input("Enter nationality [cyan](USA/FIN/CAN/SWE/CZE/RUS/SLO/FRA/GBR/SVK/DEN/NED/AUT/BLR/GER/SUI/NOR/UZB/LAT/AUS)[/cyan] [magenta]blank to stop:[/magenta]")
+        if nationality == "":
+            break
+        players = stats.top_scorers_by_nationality(nationality)
 
-    print("Players from FIN:")
-
-    for player in players:
-        print(player)
+        res = Printer(players, season, nationality)
+        res.print_players()
 
 main()
---
+++ viikko2/nhl-reader/src/printing.py
@@ -0,0 +1,32 @@
+from rich.console import Console
+from rich.table import Table
+
+class Printer:
+    def __init__(self, players, season, nationality):
+        self.players = players
+        self.season = season
+        self.nationality = nationality
+    
+    def print_players(self):
+        table = Table(title=f"Season {self.season} players from {self.nationality}")
+
+        table.add_column("Name", justify="left", style="cyan", no_wrap=True)
+        table.add_column("Teams", style="magenta")
+        table.add_column("Goals", justify="right", style="green")   
+        table.add_column("Assists", justify="right", style="green")
+        table.add_column("Points", justify="right", style="green")
+
+        for player in self.players:
+            table.add_row(
+                player.name,
+                player.team,
+                str(player.goals),
+                str(player.assists),
+                str(player.points)
+            )
+
+        console = Console()
+        console.print(table)   
+
+
+
