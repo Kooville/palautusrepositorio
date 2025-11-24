627aec37b65fe47572ffd2af49f2795f3e7c54f2
Kooville <ville.kuhlberg@helsinki.fi>
2025-11-24 21:43:30 +0200

    Tehtävät 1-3

--- viikko4/maksukortti-mock/src/tests/kassapaate_test.py
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -23,3 +23,19 @@ class TestKassapaate(unittest.TestCase):
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+
+    def test_lataa_kortille(self):
+        maksukortti_mock = Mock()
+        summa = 10
+        
+        self.kassa.lataa(maksukortti_mock, summa)
+
+        maksukortti_mock.lataa.assert_called_with(summa)
+
+    def test_ei_lataa_negatiivista(self):
+        maksukortti_mock = Mock()
+        summa = -10
+
+        self.kassa.lataa(maksukortti_mock, summa)
+
+        maksukortti_mock.lataa.assert_not_called()
